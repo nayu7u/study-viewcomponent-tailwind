526548980c1bf2c908752d72091159f55b64f2c3
nayu7u <19748682+nayu7u@users.noreply.github.com>
2023-12-30 22:16:05 +0900

    update sidebar component

--- test/components/previews/sidebar_component_preview.rb
+++ test/components/previews/sidebar_component_preview.rb
@@ -2,6 +2,15 @@
 
 class SidebarComponentPreview < ViewComponent::Preview
   def default
-    render(SidebarComponent.new(links: "links"))
+    render(
+      SidebarComponent.new(
+        links: [
+          ["Google", "https://www.google.co.jp/"],
+          ["Amazon", "https://www.amazon.co.jp/"],
+          ["Facebook", "https://www.facebook.com/"],
+          ["Apple", "https://www.apple.com/"],
+        ],
+      )
+    )
   end
 end
